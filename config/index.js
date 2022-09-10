e93e870ff4a469b361e71a92666260614e66e439
邹存安 <872377689@qq.com>
2022-09-10 18:33:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,9 +23,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "nxGnVM15euDmenqZtpW_J6OREyojZVF6z_ZVpBdG_aM",
       // 所在省份
-      province: "江苏",
+      province: "广东",
       // 所在城市
-      city: "苏州",
+      city: "惠州",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '06-14',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
