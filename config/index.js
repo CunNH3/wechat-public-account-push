6f7e4d3366c9e0ecd7543b7e190d44f37ec81a26
邹存安 <872377689@qq.com>
2022-09-10 18:28:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,21 +13,7 @@ export const config = {
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "e54d0278469e676b8037b12352b85765",
 
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "老婆0",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   },
-   * ]
-   */
+
   USERS: [
     {
       // 想要发送的人的名字
@@ -72,7 +58,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "lkxQV-5lIUcO5S3LWEG6KxhpTZ9B1i0I372Y7avwgW8",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -80,7 +66,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oIT0I6nH_5D4ouJJFNWBkTWFDIRw",
     },
   ],
 
